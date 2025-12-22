d809bffc93c950cff21f74157a15f776da9e9a90
Johan PIQUET <johan.piquet@gmail.com>
2025-12-22 19:06:45 +0100

    Correction translation

--- src/@linker/typeTranslation.ts
+++ src/@linker/typeTranslation.ts
@@ -240,7 +240,7 @@ export class TypeTranslation extends AliasType {
             map += "\n};";
 
             srcCode += `\n${map}\n\nexport default function get(lang?: string) { return lang ? (((byLang as any)[lang]) || defaultLang) :  defaultLang }`;
-            dstCode += `\n${map}\n\nexport default function get(lang) { return lang ? (((byLang as any)[lang]) || defaultLang) :  defaultLang }`;
+            dstCode += `\n${map}\n\nexport default function get(lang) { return lang ? (byLang[lang] || defaultLang) :  defaultLang }`;
 
             await writer.writeCodeFile({
                 fileInnerPath: jk_fs.join(dirName, "index"),
