34257e5bbadd1750f4b6145fb3904325907a411a
Johan PIQUET <johan.piquet@gmail.com>
2026-01-24 22:30:15 +0100

    correction

--- src/@generated/dataTable.ts
+++ src/@generated/dataTable.ts
@@ -1,24 +1,35 @@
-import type { IActionContext, JopiDataTable, JDataTable, JActionResult } from "jopi-toolkit/jk_data";
+import type { IActionContext, JopiDataTable, JDataTable, JActionResult, JDataReadParams, JDataReadResult } from "jopi-toolkit/jk_data";
+import type { Schema } from "jopi-toolkit/jk_schema";
 import type { JopiRequest, JopiTableServerActions } from "jopijs";
 
 export function toDataTable(ds: JopiDataTable, name: string, serverActions?: JopiTableServerActions): JDataTable {
-    return {
-        ...ds, name,
+    return new DataTableWrapper(ds, name, serverActions);
+}
 
-        executeAction: async (rows: any[], actionName: string, context?: IActionContext): Promise<JActionResult | void> => {
-            const req = context as unknown as JopiRequest;
-            const action = serverActions?.[actionName];
+class DataTableWrapper implements JDataTable  {
+    public readonly schema: Schema;
+    
+    constructor(private readonly ds: JopiDataTable, public readonly name: string, public readonly serverActions?: JopiTableServerActions) {
+        this.schema = ds.schema;
+    }
 
-            if (!action) {
-                return {
-                    isOk: false,
-                    errorCode: "ACTION_NOT_FOUND",
-                    errorMessage: `Action "${actionName}" not found`
-                };
-            }
+    read(params: JDataReadParams): Promise<JDataReadResult> {
+        return this.ds.read(params);
+    }
 
-            let res = await action(req, rows);
-            if (res) return res;
+    async executeAction(rows: any[], actionName: string, context?: IActionContext): Promise<JActionResult | void> {
+        const req = context as unknown as JopiRequest;
+        const action = serverActions?.[actionName];
+
+        if (!action) {
+            return {
+                isOk: false,
+                errorCode: "ACTION_NOT_FOUND",
+                errorMessage: `Action "${actionName}" not found`
+            };
         }
-    };
+
+        let res = await action(req, rows);
+        if (res) return res;
+    }
 }
